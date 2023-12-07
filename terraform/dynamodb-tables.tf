b6b3b93946475ca065ea8d103b26d53d8624b1bb
Devendra Govil <govildev@gmail.com>
2023-12-07 04:15:33 -0800

    Adding terraform file for dynamodb; adding .env.skeleton file for skeleton for .env

--- terraform/dynamodb-tables.tf
+++ terraform/dynamodb-tables.tf
@@ -1,12 +1,12 @@
-# resource "aws_dynamodb_table" "anoobix-dynamodb" {
-#   name           = "trip-stream"
-#   billing_mode   = "PROVISIONED"
-#   read_capacity  = 10
-#   write_capacity = 5
-#   hash_key       = "timestamp_id"
+resource "aws_dynamodb_table" "anoobix-dynamodb-triptable" {
+  name           = "trip-stream"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 5
+  hash_key       = "timestamp_id"
 
-#   attribute {
-#     name = "timestamp_id"
-#     type = "S"
-#   }
-# }
+  attribute {
+    name = "timestamp_id"
+    type = "S"
+  }
+}
